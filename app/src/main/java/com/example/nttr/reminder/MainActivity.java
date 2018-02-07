de384a97860dd563b9267d444ec75a1626ab2e18
RikimaruKOIDE <koide@nttr.co.jp>
2018-02-07 16:47:22 +0900

    編集画面レイアウトパーツ配置

--- app/src/main/java/com/example/nttr/reminder/MainActivity.java
+++ app/src/main/java/com/example/nttr/reminder/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
             "German"
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
